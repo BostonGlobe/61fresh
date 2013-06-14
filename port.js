7152f379a2bfab3b612a6793d467ba256a11dd03
Daniel McLaughlin <daniel.mclaughlin@boston.com>
2013-06-14 16:49:28 -0400

    Destroy/create lists more optimally

--- port.js
+++ port.js
@@ -40,45 +40,60 @@ var portUsers = function() {
 		});
 };
 
-var list_destroy_pointer = 0;
 
-var destroyLists = function() {
-	console.log(list_destroy_pointer);
-	var params = {owner_screen_name: my_screen_name, slug: 'a'+list_destroy_pointer};
-	T.post('lists/destroy', params,
-		function(err, reply) {
-			if (err) {
-				console.log('lists/destroy');
-				console.log(params);
-				console.log(err);
-			} else {
-				list_destroy_pointer++;
-				if (list_destroy_pointer === 1000) {
-					clearInterval(destroyTrigger);
+var destroyAllLists = function() {
+	var listsToDestroy;
+	var zapList = function() {
+		var list_id = listsToDestroy.pop().id_str;
+		console.log(listsToDestroy.length);
+		T.post('lists/destroy', {list_id:list_id},
+			function(err, reply) {
+				if (err) {
+					console.log('lists/destroy '+list_id);
+					console.log(err);
+					setTimeout(zapList,500);
+				} else {
+					setTimeout(zapList,0);
 				}
-			}
-		});
+			});
+	}
+	T.get('lists/ownerships',{screen_name:my_screen_name,count:1000},function(e,r) {
+		listsToDestroy = r.lists;
+		setTimeout(zapList,0);
+	});
 };
 
-var list_create_pointer = 0;
-
-var createLists = function() {
-	console.log(list_create_pointer);
-	var params = {name: 'a'+list_create_pointer, mode:'private'};
-	T.post('lists/create', params,
-		function(err, reply) {
-			if (err) {
-				console.log('lists/create');
-				console.log(params);
-				console.log(err);
-			} else {
-				list_create_pointer++;
-				if (list_create_pointer === 1000) {
-					clearInterval(createTrigger);
-				}
+var createAllLists = function() {
+	var list_create_pointer = 0;
+	var createList = function() {
+		// console.log(list_create_pointer);
+		if (_.find(existing_lists,function(d) {return 'a'+list_create_pointer === d.name})===undefined) {
+			console.log(list_create_pointer);
+			var params = {name: 'a'+list_create_pointer, mode:'private'};
+			T.post('lists/create', params,
+				function(err, reply) {
+					if (err) {
+						console.log('lists/create');
+						console.log(params);
+						console.log(err);
+					}
+					list_create_pointer++;
+					if (list_create_pointer < 1000) {
+						setTimeout(createList,0);
+					}
+				});
+		} else {
+			list_create_pointer++;
+			if (list_create_pointer < 1000) {
+				setTimeout(createList,0);
 			}
-		});
+		}
+	};
+	var existing_lists;
+	T.get('lists/ownerships',{screen_name:my_screen_name,count:1000},function(e,r) {
+		existing_lists = r.lists;
+		setTimeout(createList,0);
+	});
 };
 
-// var destroyTrigger = setInterval(destroyLists,(15*60*1000)/175);
-// var createTrigger = setInterval(createLists,(15*60*1000)/175);
+
