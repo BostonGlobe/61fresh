af507403ea9e319b983e50dd2378dfc389db8870
Daniel McLaughlin <daniel.mclaughlin@boston.com>
2013-06-15 21:36:29 -0400

    Add routines to "top off" altready existing lists if twitter missed a bunch of users

--- port.js
+++ port.js
@@ -96,4 +96,53 @@ var createAllLists = function() {
 	});
 };
 
+var refillList = function(list_index) {
+	var list_members;
+	var users_per_fill = 30;
+	var per_fill_backoff = 10;
+	var addMore = function() {
+		var uids = _.first(list_members,users_per_fill);
+		if (uids.length > 0) {
+			console.log("Adding " + uids.length + " of " + list_members.length + " to list a" + list_index + ".");
+			var params = {owner_screen_name: my_screen_name, slug: 'a'+list_index, user_id: uids};
+			T.post('lists/members/create_all', params,
+				function (err,reply) {
+					if (err) {
+						console.log('lists/members/create_all');
+						console.log(err);
+						users_per_fill = Math.max(1,users_per_fill-per_fill_backoff);
+						setTimeout(addMore,1000);
+					} else {
+						list_members = _.rest(list_members,uids.length);
+						users_per_fill = Math.min(100,users_per_fill+1);
+						setTimeout(addMore,0);
+					}
+				});
+		}
+	}
+	sql_conn.query("SELECT user_id FROM users WHERE ?", {list_id:list_index},
+		function(e,r) {
+			list_members = _.pluck(r,'user_id');
+			setTimeout(addMore,0);
+		});
+}
+
+var refillAllLists = function() {
+	sql_conn.query("SELECT list_id, COUNT(*) as num FROM users WHERE list_id IS NOT NULL GROUP BY list_id",
+		function(e,sql_lists) {
+			T.get('lists/ownerships',{screen_name:my_screen_name,count:1000},
+				function(e,r) {
+					var twitter_lists = r.lists;
+					sql_lists.forEach(
+						function(this_list) {
+							var twitter_member_count = _.find(twitter_lists,function(d) {return 'a'+this_list.list_id === d.name}).member_count;
+							if ((this_list.num-twitter_member_count) > 100) {
+								console.log("Bad: " + this_list.list_id);
+								refillList(this_list.list_id);
+							} 
+						});
+				});
+		});
+}
+
 
