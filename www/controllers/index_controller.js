2007afcd580c362c938d8b5e25cade2bd69ac928
Chris Marstall <chris@psychoastronomy.org>
2013-08-05 13:15:37 -0400

    added tweet headlines view

--- www/controllers/index_controller.js
+++ www/controllers/index_controller.js
@@ -6,18 +6,42 @@ IndexController = function()
 		this.json = json
 		titles = {}
 		that=this
+
+		// loop through & process articles
 		_.each(this.json.articles,function(article){
+			
+			// find earliest tweet, make it the 'author' tweet
+			article.first_tweeter=article.tweeters[1]
+			_.each(article.tweets,function(tweet,i){
+				if (new Date(tweet.created_at).getTime()<new Date(first_tweeter).created_at.getTime()) article.first_tweeter = tweet
+			})
+			// find and combine duplicate articles
 			if (titles[article.title]) // found a dupe
 			{
 				if (article.source!=titles[article.title].source) titles[article.title].source +=", "+article.source
+				article_to_use = titles[article.title]
+				_.each(article.tweeters,function(tweeter){
+					article_to_use.tweeters.push(tweeter)
+				})
 				article.deleted=true
 				return; 
 			}
 			titles[article.title]=article;
-			if ((new Date().getTime()-new Date(article.first_tweeted).getTime())<30*60*1000) article.is_new=true // 1 hour
+
+			// replace headline with text of tweet by user with most followers
+			tweet_title = article.first_tweeter.text
+			article.tweet_title = tweet_title.replace (/http[^\s]+/g,"")
+			article.tweet_title_screen_name = article.first_tweeter.screen_name
+			article.profile_image_url = article.first_tweeter.profile_image_url
+
+			// mark new articles as new
+			if ((new Date().getTime()-new Date(article.first_tweeted).getTime())<60*60*1000) article.is_new=true // 1 hour
 			else article.is_new=false
 		})
-		this.render('index')
+		view = this.query_param("view")
+		if (view==null) view='index'
+		
+		this.render("index",view)
 	}
 
 	this.start = function()
