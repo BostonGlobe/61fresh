b07e41bb2c0b4bc10e4fed63f63576d2ef79ed60
Chris Marstall <chris@psychoastronomy.org>
2013-08-16 11:17:48 -0400

    stuff

--- www/commit_and_push.sh
+++ www/commit_and_push.sh
@@ -1,3 +1,6 @@
+pushd .
+cd ~/condor
 git commit -am "$1"
 git push origin master
 ./push.sh
+popd
--- www/push.sh
+++ www/push.sh
@@ -1,2 +1,3 @@
+cd ~/condor/www
  s3cmd --add-header "Cache-Control: max-age=60" --recursive put --acl-public --guess-mime-type controllers css feed.html index.html js piggyback templates s3://condor.globe.com
 ssh -t labstation@***REMOVED*** "cd condor;git pull"
