9394862941feb35f9f3f7be10c878da4afe6cb05
Chris Marstall <chris@psychoastronomy.org>
2013-08-16 11:08:13 -0400

    added push to labstation

--- www/push.sh
+++ www/push.sh
@@ -1,2 +1,2 @@
  s3cmd --add-header "Cache-Control: max-age=60" --recursive put --acl-public --guess-mime-type controllers css feed.html index.html js piggyback templates s3://condor.globe.com
-
+ssh -t labstation@***REMOVED*** "cd condor;git pull"
