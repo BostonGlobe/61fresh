68007e398feb0ec5059cf1fdb19405fd06674c4b
Chris Marstall <chris@psychoastronomy.org>
2013-09-11 14:07:07 -0400

    about page

--- push.sh
+++ push.sh
@@ -8,6 +8,6 @@ git push origin master
 ssh -t ec2-user@ec2-54-242-45-233.compute-1.amazonaws.com "cd condor;git pull"
 
 cd $CONDOR_HOME/www
-s3cmd --add-header "Cache-Control: max-age=60" --recursive put --acl-public --guess-mime-type controllers css feed.html index.html homepage.html js piggyback templates images $bucket_name
+s3cmd --add-header "Cache-Control: max-age=60" --recursive put --acl-public --guess-mime-type controllers css feed.html index.html about.html homepage.html js piggyback templates images $bucket_name
 
 popd
