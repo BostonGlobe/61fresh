e4a805c73c7b460202d0f417d4a18ebc516da73d
Daniel McLaughlin <daniel.mclaughlin@boston.com>
2013-10-04 21:56:28 -0400

    Turn off node ingest

--- run.js
+++ run.js
@@ -365,8 +365,8 @@ var refreshUsers = function() {
 };
 
 
-var refreshBostonTrigger = setInterval(refreshBoston,(15*60*1000)/175);
-var refreshListsTrigger = setInterval(refreshLists,(15*60*1000)/179.5);
+// var refreshBostonTrigger = setInterval(refreshBoston,(15*60*1000)/175);
+// var refreshListsTrigger = setInterval(refreshLists,(15*60*1000)/179.5);
 var relationsTrigger = setInterval(startRelationships,61*1000);
 var refreshUsersTrigger = setInterval(refreshUsers,(15*60*1000)/176);
 fillLists();
