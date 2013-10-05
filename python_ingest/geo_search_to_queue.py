247a129a4384333cc81817e3c7a79a1a08d07958
EC2 Default User <ec2-user@ip-10-234-25-69.ec2.internal>
2013-10-05 11:53:31 +0000

    add timeout to geosearch

--- python_ingest/geo_search_to_queue.py
+++ python_ingest/geo_search_to_queue.py
@@ -17,8 +17,9 @@ search_since_id = '0'
 @mainloop
 def do_search():
         global search_since_id
-        response = limited_search(geocode='42.3583,-71.0603,10mi', result_type= 'recent', count=100, since_id=search_since_id)
+        response = limited_search(geocode='42.3583,-71.0603,10mi', result_type= 'recent', count=100, since_id=search_since_id,_timeout=15)
         tweets = [tweet.get('retweeted_status',tweet) for tweet in response['statuses']]
+	print "geo search got %s tweets" % len(tweets)
         tq.enqueueTweets(tweets)
         uq.enqueueUsers([tweet['user']['id'] for tweet in tweets])
         search_since_id = response['search_metadata']['max_id_str']
